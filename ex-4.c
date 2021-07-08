a7c0446478a869472058f47508ef48b1618e5de7
Yuka <s1280157@u-aizu.ac.jp>
2021-07-08 19:35:34 +0900

    Initial revision

---
+++ ex-4.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main(){
+  int value1, value2, sum;
+
+  srand(time(NULL));
+  value1 = rand() % 6 + 1;
+  value2 = rand() % 6 + 1;
+
+  printf("Die 1: %d\n", value1);
+  printf("Die 2: %d\n", value2);
+  printf("Total value: %d\n", sum = value1 + value2);
+
+  return 0;
+
+}
