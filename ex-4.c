e2a4bbdee185bced45688d56f02235120f7b7516
Yuka <s1280157@u-aizu.ac.jp>
2021-07-08 19:40:28 +0900

    Won/lost message added

--- ex-4.c
+++ ex-4.c
@@ -13,6 +13,9 @@ int main(){
   printf("Die 2: %d\n", value2);
   printf("Total value: %d\n", sum = value1 + value2);
 
+  if(sum>7) printf("%s won!\n", name);
+  else printf("%s lost!\n", name);
+
   return 0;
 
 }
